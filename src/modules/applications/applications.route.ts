6c48aa93be896572ca2fa1d37f51498484166116
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2023-09-26 11:40:45 +0530

    Refactor.

--- src/modules/applications/applications.route.ts
+++ src/modules/applications/applications.route.ts
@@ -2,13 +2,9 @@ import { AppServerType } from "@/lib/server";
 import ApplicationsService from "./application.service";
 import ApplicationsController from "./applications.controller";
 
-let applicationService: ApplicationsService;
-let applicationsController: ApplicationsController;
-
 export const applicationRoutes = async (fastify: AppServerType) => {
-  if (!applicationService) applicationService = new ApplicationsService();
-  if (!applicationsController)
-    applicationsController = new ApplicationsController(applicationService);
+  const applicationService = new ApplicationsService();
+  const applicationsController = new ApplicationsController(applicationService);
 
   fastify.get("/", applicationsController.getAllApplications);
   fastify.post("/", applicationsController.createApplication);
--- src/modules/roles/roles.route.ts
+++ src/modules/roles/roles.route.ts
@@ -2,12 +2,9 @@ import { AppServerType } from "@/lib/server";
 import RolesController from "./roles.controller";
 import RolesService from "./roles.service";
 
-let rolesService: RolesService;
-let rolesController: RolesController;
-
 export const rolesRoutes = async (fastify: AppServerType) => {
-  if (!rolesService) rolesService = new RolesService();
-  if (!rolesController) rolesController = new RolesController(rolesService);
+  const rolesService = new RolesService();
+  const rolesController = new RolesController(rolesService);
 
   fastify.post("/", rolesController.createRole);
 };
